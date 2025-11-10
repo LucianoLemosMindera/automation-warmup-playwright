0e28333906ca35d04e55c8fa28be796ddc5f33f0
Luciano Lemos <luciano.lemos@mindera.com>
2025-11-10 22:28:48 +0000

    validating dynamic table

---
+++ tests/table.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import hpCharacters from './data/json/hp.characters.json'
+
+for(const hpCharacter of hpCharacters){
+    test('Characte '+ hpCharacter.name, async ({ page }) => {
+        await page.goto('/table');
+
+        const nameWithoutSpace = hpCharacter.name.replace(' ', '');
+
+        await expect(page.locator('#tableCharacterName' + nameWithoutSpace))
+            .toBeVisible();
+        await expect(page.getByRole('img', {name:  hpCharacter.name}))
+            .toBeVisible();
+        await expect(page.locator('#tableCharacterHouse' + nameWithoutSpace))
+            .toBeVisible();
+
+        const birth = hpCharacter.dateOfBirth ? hpCharacter.dateOfBirth : 'Unknown';
+        await expect(page.getByRole('cell', {name: birth}))
+            .toBeVisible();
+        await expect(page.locator('#tableCharacterActor' + nameWithoutSpace))
+            .toBeVisible();
+
+        console.log(hpCharacters);
+    });
+}
+
+
